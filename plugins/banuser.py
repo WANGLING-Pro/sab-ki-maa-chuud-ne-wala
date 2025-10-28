60797d5eebe3adfee7b44b9818a3d455cb2a7a6b
WANGLING-Pro <realidhai563@gmail.com>
2025-10-28 12:35:28 +0530

    Update banuser.py

--- plugins/banuser.py
+++ plugins/banuser.py
@@ -1,11 +1,11 @@
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
+# Don't Remove Credit @P_World_18, @i_am_nerev_die
+# Ask Doubt on telegram @Upcoming
 #
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+# Copyright (C) 2025 by WANGLING-Pro@Github, < https://github.com/WANGLING-Pro >.
 #
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# This file is part of < https://github.com/WANGLING-Pro/sab-ki-maa-chuud-ne-wala > project,
 # and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+# Please see < https://github.com/WANGLING-Pro/sab-ki-maa-chuud-ne-wala >
 #
 # All rights reserved.
 #
