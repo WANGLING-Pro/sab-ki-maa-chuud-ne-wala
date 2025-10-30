e59ff0957ac0e9a80266543151ffd03ba9200f58
WANGLING-Pro <realidhai563@gmail.com>
2025-10-30 19:20:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from threading import Thread
+from click_counter import click_app
 from bot import Bot
 import pyrogram.utils
 
 pyrogram.utils.MIN_CHANNEL_ID = -1002917577512
 
 if __name__ == "__main__":
+    # Flask app ko background me run karna
+    Thread(target=lambda: click_app.run(host="0.0.0.0", port=10000)).start()
+
+    # Telegram bot run karna
     Bot().run()
