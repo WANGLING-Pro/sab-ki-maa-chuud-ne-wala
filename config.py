2005d2a35f570301d032024514ed26632e9f6194
WANGLING-Pro <realidhai563@gmail.com>
2025-12-16 12:19:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,13 +15,14 @@ from os import environ,getenv
 import logging
 from logging.handlers import RotatingFileHandler
 
-#I_am_nerev_die on Tg
-#--------------------------------------------
+#-------------------------------------------------------#
+MSG_EFFECT = 5104841245755180586
+#-------------------------------------------------------#
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339:")
 APP_ID = int(os.environ.get("APP_ID", "23552440")) #Your API ID from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "8607b5532f753811597355b8e9a7be22") #Your API Hash from my.telegram.org
-#--------------------------------------------
+#------------------------------------------------------#
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "")) #Your db channel Id
 OWNER = os.environ.get("OWNER", "I_am_nerev_die") # Owner username without @
