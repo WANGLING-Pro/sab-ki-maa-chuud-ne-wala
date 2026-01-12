3633ca763960354b7347fa56abbb28402a975f92
WANGLING-Pro <realidhai563@gmail.com>
2026-01-12 23:18:00 +0530

    Update Channel_post.py

--- plugins/Channel_post.py
+++ plugins/Channel_post.py
@@ -35,11 +35,11 @@ async def channel_post(client: Client, message: Message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 
     await reply_text.edit(
-    f"<blockquote><b>Here is your link</b>\n\n{link}</blockquote>",
+    f"<blockquote><b>Here is your link</b></blockquote>\n<blockquote>{link}</blockquote>",
     reply_markup=reply_markup,
     disable_web_page_preview=True,
     parse_mode="HTML"
-    )
+                           )
     
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
