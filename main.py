331db3046b22c3634e0e3b00d27592cb17b69725
WANGLING-Pro <realidhai563@gmail.com>
2026-01-17 17:50:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import threading
+import os
+from flask import Flask
 from bot import Bot
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "OK"
+
+def run_flask():
+    port = int(os.getenv("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 def run_bot():
     bot = Bot()
     bot.run()
 
 if __name__ == "__main__":
+    threading.Thread(target=run_flask).start()
     run_bot()
