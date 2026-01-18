ee80b9a844092dfcb078b3c3c7ed67d7ba4af335
WANGLING-Pro <realidhai563@gmail.com>
2026-01-18 13:05:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
-import os
 import threading
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+from click_counter import click_app
 from bot import Bot
 
-# -----------------------------
-# Dummy HTTP Server
-# -----------------------------
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Bot is running.")
+def run_flask():
+    port = int(os.getenv("PORT", 8000))
+    click_app.run(host="0.0.0.0", port=port)
 
-def run_http():
-    port = int(os.environ.get("PORT", 8000))
-    server = HTTPServer(("0.0.0.0", port), Handler)
-    print(f"Dummy HTTP server running on port {port}")
-    server.serve_forever()
-
-# -----------------------------
-# Run Bot
-# -----------------------------
 def run_bot():
     bot = Bot()
     bot.run()
 
-# -----------------------------
-# Start Both
-# -----------------------------
 if __name__ == "__main__":
-    t = threading.Thread(target=run_http)
+    t = threading.Thread(target=run_flask)
     t.start()
-
     run_bot()
