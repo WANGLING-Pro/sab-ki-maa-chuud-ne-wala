24907ef4dfd8423bf00b1cd44534a3db7488c23f
WANGLING-Pro <realidhai563@gmail.com>
2026-03-21 11:37:02 +0530

    Simplify main execution by removing Flask thread
    Removed threading to run Flask and simplified execution flow.

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import threading
-import os
-# from click_counter import click_app
 from bot import Bot
 
-def run_flask():
-    port = int(os.getenv("PORT", 8000))
-    click_app.run(host="0.0.0.0", port=port)
-
-def run_bot():
+if __name__ == "__main__":
     bot = Bot()
     bot.run()
-
-if __name__ == "__main__":
-    t = threading.Thread(target=run_flask)
-    t.start()
-    run_bot()
