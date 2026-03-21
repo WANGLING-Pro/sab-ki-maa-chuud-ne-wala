19f2cf35d04786634d465004276f209c9f3b0dd0
WANGLING-Pro <realidhai563@gmail.com>
2026-03-21 18:50:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import threading
+import os
+
+from click_counter import click_app
+
 from bot import Bot
 
-if __name__ == "__main__":
-    bot = Bot()
-    bot.run()
+def run_flask():
+port = int(os.getenv("PORT", 8000))
+click_app.run(host="0.0.0.0", port=port)
+
+def run_bot():
+bot = Bot()
+bot.run()
+
+if name == "main":
+t = threading.Thread(target=run_flask)
+t.start()
+run_bot()
+
+main.py hai
